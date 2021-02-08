cf05cf08c92644869c57a8a526304152f4ff9171
obus <alexander.senov@gmail.com>
2021-02-08 20:16:19 +0300

    updated setup.py to include package sources into the build distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import find_packages, setup
 
+pkg_name = 'optimizationalgorithmscomparison'
+
 setup(
-    name='optimizationalgorithmscomparison',
+    name=pkg_name,
     version='0.1.0',
-    packages=find_packages(include=['optimizationalgorithmscomparison']),
+    packages=find_packages(include=[pkg_name], where='src'),
     description='Optimization algorithms comparison library',
     author='Andrei Telbukhov',
     lisence='',
-    install_requires=['numpy']
+    python_requires='>=3.6',
+    install_requires=[
+    	'numpy',
+    ],
+    package_dir={
+        '': 'src',
+    }
 )
