6431c53e8f52400ce997564502513b7fa476f8b0
Drucetination <st050025@student.spbu.ru>
2021-02-08 18:30:26 +0300

    added conjugate gradient method

--- src/optimizationalgorithmscomparison/functions.py
+++ src/optimizationalgorithmscomparison/functions.py
@@ -21,12 +21,29 @@ class QuadraticFunction:
             return self.a * x - self.b
 
 
-def gradient_descent(fun, x0, step=None, epsilon=1e-5):
-    x_1 = x0
-    x_2 = x0 + 1000.
-    it = 0
+def gd(fun, x_0, step=None, epsilon=1e-3):
+    x_1 = x_0
+    x_2 = x_0 + 1000.
     while np.linalg.norm(fun.evaluate(x_1) - fun.evaluate(x_2)) > epsilon:
         x_2 = np.copy(x_1)
         x_1 -= step * fun.gradient(x_1)
+
+    return x_1
+
+
+def cg(fun, x_0, epsilon=1e-3):
+    x_1 = x_0
+    x_2 = x_0 + 1000.
+    d = -fun.gradient(x_0)
+    r_1 = np.copy(d)
+    r_2 = np.copy(d)
+    while np.linalg.norm(fun.evaluate(x_1) - fun.evaluate(x_2)) > epsilon:
+        x_2 = np.copy(x_1)
+        alpha = np.matmul(r_1.T, r_1) / np.matmul(np.matmul(d.T, fun.a), d)
+        x_1 += alpha * d
+        r_2 -= alpha * np.matmul(fun.a, d)
+        beta = np.matmul(r_2.T, r_2) / np.matmul(r_1.T, r_1)
+        d = r_2 + beta * d
+        r_1 = np.copy(r_2)
 
     return x_1
