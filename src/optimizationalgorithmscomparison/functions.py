40c8f4d334bc2edf44ce1226a4ee4f5c990e5edf
Drucetination <st050025@student.spbu.ru>
2021-02-08 19:34:09 +0300

    Added newton method Defined optimization methods as classes

--- src/optimizationalgorithmscomparison/functions.py
+++ src/optimizationalgorithmscomparison/functions.py
@@ -1,7 +1,23 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
 
-class QuadraticFunction:
+class Function(ABC):
+
+    @abstractmethod
+    def evaluate(self, x):
+        pass
+
+    @abstractmethod
+    def gradient(self, x):
+        pass
+
+    @abstractmethod
+    def hessian(self, x):
+        pass
+
+
+class QuadraticFunction(Function):
 
     def __init__(self, a, b, c):
         self.a = a
@@ -20,30 +36,69 @@ class QuadraticFunction:
         else:
             return self.a * x - self.b
 
-
-def gd(fun, x_0, step=None, epsilon=1e-3):
-    x_1 = x_0
-    x_2 = x_0 + 1000.
-    while np.linalg.norm(fun.evaluate(x_1) - fun.evaluate(x_2)) > epsilon:
-        x_2 = np.copy(x_1)
-        x_1 -= step * fun.gradient(x_1)
-
-    return x_1
+    def hessian(self, x):
+        return self.a
 
 
-def cg(fun, x_0, epsilon=1e-3):
-    x_1 = x_0
-    x_2 = x_0 + 1000.
-    d = -fun.gradient(x_0)
-    r_1 = np.copy(d)
-    r_2 = np.copy(d)
-    while np.linalg.norm(fun.evaluate(x_1) - fun.evaluate(x_2)) > epsilon:
-        x_2 = np.copy(x_1)
-        alpha = np.matmul(r_1.T, r_1) / np.matmul(np.matmul(d.T, fun.a), d)
-        x_1 += alpha * d
-        r_2 -= alpha * np.matmul(fun.a, d)
-        beta = np.matmul(r_2.T, r_2) / np.matmul(r_1.T, r_1)
-        d = r_2 + beta * d
-        r_1 = np.copy(r_2)
+class OptimizationMethod(ABC):
 
-    return x_1
+    @abstractmethod
+    def run(self, x_0):
+        pass
+
+
+class Newton(OptimizationMethod):
+
+    def __init__(self, fun, epsilon=1e-3):
+        self.fun = fun
+        self.epsilon = epsilon
+
+    def run(self, x_0):
+        x_1 = x_0
+        x_2 = x_0 + 1000.
+        while np.linalg.norm(self.fun.evaluate(x_1) - self.fun.evaluate(x_2)) > self.epsilon:
+            x_2 = np.copy(x_1)
+            x_1 -= np.matmul(np.linalg.inv(self.fun.hessian(x_1)), self.fun.gradient(x_1))
+
+        return x_1
+
+
+class GD(OptimizationMethod):
+
+    def __init__(self, fun, epsilon=1e-3, step=None):
+        self.fun = fun
+        self.epsilon = epsilon
+        self.step = step
+
+    def run(self, x_0):
+        x_1 = x_0
+        x_2 = x_0 + 1000.
+        while np.linalg.norm(self.fun.evaluate(x_1) - self.fun.evaluate(x_2)) > self.epsilon:
+            x_2 = np.copy(x_1)
+            x_1 -= self.step * self.fun.gradient(x_1)
+
+        return x_1
+
+
+class CG(OptimizationMethod):
+
+    def __init__(self, fun, epsilon):
+        self.fun = fun
+        self.epsilon = epsilon
+
+    def run(self, x_0):
+        x_1 = x_0
+        x_2 = x_0 + 1000.
+        d = -self.fun.gradient(x_0)
+        r_1 = np.copy(d)
+        r_2 = np.copy(d)
+        while np.linalg.norm(self.fun.evaluate(x_1) - self.fun.evaluate(x_2)) > self.epsilon:
+            x_2 = np.copy(x_1)
+            alpha = np.matmul(r_1.T, r_1) / np.matmul(np.matmul(d.T, self.fun.a), d)
+            x_1 += alpha * d
+            r_2 -= alpha * np.matmul(self.fun.a, d)
+            beta = np.matmul(r_2.T, r_2) / np.matmul(r_1.T, r_1)
+            d = r_2 + beta * d
+            r_1 = np.copy(r_2)
+
+        return x_1
