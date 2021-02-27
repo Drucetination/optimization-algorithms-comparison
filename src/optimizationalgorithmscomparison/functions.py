377869777176cbb5a7f19880860d674f3d5321ea
Drucetination <st050025@student.spbu.ru>
2021-02-27 03:18:02 +0300

    Added criterion class added simple visualization added some statistics

--- src/optimizationalgorithmscomparison/functions.py
+++ src/optimizationalgorithmscomparison/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
 
 from abc import ABC, abstractmethod
 
@@ -28,9 +29,9 @@ class QuadraticFunction(Function):
 
     def evaluate(self, x):
         if x.shape[0] > 1:
-            return 0.5 * np.matmul(x.T, np.matmul(self.a, x)) + np.matmul(self.b.T, x) + self.c
+            return (0.5 * np.matmul(x.T, np.matmul(self.a, x)) + np.matmul(self.b.T, x) + self.c)[0][0]
         else:
-            return 0.5 * self.a * x ** 2 + self.b * x + self.c
+            return (0.5 * self.a * x ** 2 + self.b * x + self.c)[0][0]
 
     def gradient(self, x):
         if x.shape[0] > 1:
@@ -48,13 +49,21 @@ class OptimizationMethod(ABC):
     def run(self, fun, x_0, epsilon):
         pass
 
+    @abstractmethod
+    def step(self, *args):
+        pass
+
 
 class Newton(OptimizationMethod):
 
-    def __init__(self, criterion=None, max_iteration=10):
-        self.criterion = criterion
+    def __init__(self, max_iteration=10):
         self.max_iteration = max_iteration
-        self.name = "Newton method"
+        self.name = "Newton"
+
+    def step(self, fun, x_prev):
+        x = x_prev - np.matmul(np.linalg.inv(fun.hessian(x_prev)), fun.gradient(x_prev))
+        y = fun.evaluate(x)
+        return x, y
 
     def run(self, fun, x_0, epsilon):
         x_1 = np.copy(x_0)
@@ -70,10 +79,15 @@ class Newton(OptimizationMethod):
 
 class GD(OptimizationMethod):
 
-    def __init__(self, step=None, max_iteration=10):
+    def __init__(self, step_size=1e-3, max_iteration=10):
         self.max_iteration = max_iteration
-        self.step = step
-        self.name = "Gradient descent method"
+        self.step_size = step_size
+        self.name = "GD"
+
+    def step(self, fun, x_prev):
+        x = x_prev - self.step_size * fun.gradient(x_prev)
+        y = fun.evaluate(x)
+        return x, y
 
     def run(self, fun, x_0, epsilon):
         x_1 = np.copy(x_0)
@@ -81,7 +95,7 @@ class GD(OptimizationMethod):
         iteration = 0
         while np.linalg.norm(fun.evaluate(x_1) - fun.evaluate(x_2)) > epsilon and iteration < self.max_iteration:
             x_2 = np.copy(x_1)
-            x_1 -= self.step * fun.gradient(x_1)
+            x_1 -= self.step_size * fun.gradient(x_1)
             iteration += 1
 
         return x_1, iteration
@@ -91,7 +105,16 @@ class CG(OptimizationMethod):
 
     def __init__(self, max_iteration=10):
         self.max_iteration = max_iteration
-        self.name = "Conjugate gradients method"
+        self.name = "CG"
+
+    def step(self, fun, x_prev, d_prev, r_prev):
+        alpha = np.matmul(r_prev.T, r_prev) / np.matmul(np.matmul(d_prev.T, fun.a), d_prev)
+        x = x_prev + alpha * d_prev
+        r_next = r_prev - alpha * np.matmul(fun.a, d_prev)
+        beta = np.matmul(r_next.T, r_next) / np.matmul(r_prev.T, r_prev)
+        d = r_next + beta * d_prev
+        y = fun.evaluate(x)
+        return x, y, d, r_next
 
     def run(self, fun, x_0, epsilon):
         x_1 = np.copy(x_0)
@@ -110,24 +133,87 @@ class CG(OptimizationMethod):
             r_1 = np.copy(r_2)
             iteration += 1
 
-        return x_1, iteration-1
+        return x_1, iteration - 1
 
 
-class StopCriterion:
-    ...
+class StopCriterion(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def criterion(*args):
+        pass
 
 
-def compare(methods, target, x_0, epsilon):
+class ArgumentalCriterion(StopCriterion):
+    @staticmethod
+    def criterion(x_1, x_2, epsilon):
+        return np.linalg.norm(x_2 - x_1) > epsilon
+
+
+class FunctionalCriterion(StopCriterion):
+    def __init__(self, fun):
+        self.fun = fun
+
+    def criterion(self, x_1, x_2, epsilon):
+        return np.linalg.norm(self.fun.evaluate(x_2) - self.fun.evaluate(x_1)) > epsilon
+
+
+def compare(methods, target, x_0, epsilon, stop_criterion, max_iteration=10):
     """
     :param methods: list of optimization algorithms to compare
-    :param epsilon: accuracy
     :param target: target function
+    :param epsilon: accuracy
+    :param stop_criterion: stop criterion
     :param x_0: starting point
+    :param max_iteration: maximum number of iteration
     """
+    plot_number = 1
+
+    cols = ['method', 'iterations', 'gradients', 'hessians', 'function minimum']
+    df = pd.DataFrame(columns=cols)
 
     for method in methods:
-        print(method.name, ":")
-        minimizer, iter_num = method.run(target, x_0, epsilon)
-        minimum = target.evaluate(minimizer)
-        print("Minimum of the function is", minimum[0][0], "reached at x =", minimizer, "on the iteration",
-              iter_num)
+
+        x_1 = np.copy(x_0)
+        x_2 = x_1 + epsilon * 2
+        y_data = [target.evaluate(x_1)]
+
+        gradients = 0
+        hessians = 0
+
+        if method.name == "CG":
+            d = -1 * target.gradient(x_0)
+            gradients = 1
+            r = np.copy(d)
+            i = 0
+            while (i < max_iteration) and (stop_criterion.criterion(x_1, x_2, epsilon)):
+                x_2 = np.copy(x_1)
+                x_1, y, d, r = method.step(target, x_1, d, r)
+                y_data.append(y)
+                i += 1
+        else:
+            i = 0
+            while (i < max_iteration) and (stop_criterion.criterion(x_1, x_2, epsilon)):
+                x_2 = np.copy(x_1)
+                x_1, y = method.step(target, x_1)
+                y_data.append(y)
+                i += 1
+
+        iterations = range(i + 1)
+        if method.name == "GD" or method.name == "Newton":
+            gradients = len(iterations)
+        if method.name == "Newton":
+            hessians = len(iterations)
+
+        df_tmp = pd.DataFrame([[method.name, len(iterations), gradients, hessians, min(y_data)]], columns=cols)
+        df = df.append(df_tmp, ignore_index=True)
+
+        plt.subplot(len(methods), 1, plot_number)
+        plt.plot(iterations, y_data)
+        plt.title(method.name)  # заголовок
+        plt.xlabel("Iteration")  # ось абсцисс
+        plt.ylabel("Function value")
+        plot_number += 1
+
+    plt.show()
+    print(df)
